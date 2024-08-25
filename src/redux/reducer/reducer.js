0661cff43aa9423c03b8f8513669b714bd309aed
victoryalhj <victoryalhj@gmail.com>
2024-08-25 15:49:11 +0100

    second commit

---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,22 @@
+let initialState = {
+  count: 0,
+  id:"",
+  password:"",
+};
+
+function reducer(state = initialState, action) {
+  console.log("action?",action);
+
+  switch(action.type){
+    case "INCREMENT":
+      return { ...state, count: state.count + action.payload.num };
+    case "DECREASE":
+      return { ...state, count: state.count + action.payload.num };
+      case "LOGIN" :
+        return { ...state,id:action.payload.id,password:action.payload.password};
+      default:
+        return { ...state };
+  } 
+}
+
+export default reducer;
